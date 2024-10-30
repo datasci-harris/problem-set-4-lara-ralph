932bb90c86a59f44e98ea1beda3097b80e5d8e95
RalphValiere <ralphvaleryv@uchicago.edu>
2024-10-30 15:53:54 -0500

    Committing the initial version of the ps4.qmd file in the main branch, while creating the .gitignore to have the files to ignore.

---
+++ .vdoc.py
@@ -0,0 +1,110 @@
+# type: ignore
+# flake8: noqa
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+import os
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+import geopandas as gpd
+import shapely
+from shapely import Polygon, Point
+from numpy import mean
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
